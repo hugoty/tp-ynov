04a8e5694e62c3cc715e97c452ca0da089b2cee8
hugoty <hugo.raoult@hotmail.fr>
2023-12-01 17:02:00 +0100

    maj middleware

--- account-service/account-routes/accountRoutes.js
+++ account-service/account-routes/accountRoutes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const accountController = require("../account-controller/accountController");
+const authMiddleware = require("../../middlewares/authMiddleware"); // Middleware d'authentification
 
 // Route pour créer un nouveau compte
-router.post("/", accountController.createAccount);
+router.post("/", authMiddleware, accountController.createAccount);
 
 // Route pour obtenir les détails d'un compte
-router.get("/:accountId", accountController.getAccount);
+router.get("/:accountId", authMiddleware, accountController.getAccount);
 
 // Route pour mettre à jour un compte
-router.put("/:accountId", accountController.updateAccount);
+router.put("/:accountId", authMiddleware, accountController.updateAccount);
 
 // Route pour supprimer un compte
-router.delete("/:accountId", accountController.deleteAccount);
+router.delete("/:accountId", authMiddleware, accountController.deleteAccount);
 
 module.exports = router;
--- transaction-service/transaction-routes/transactionRoutes.js
+++ transaction-service/transaction-routes/transactionRoutes.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const transactionController = require("../transaction-controller/transactionController");
+const authMiddleware = require("../../middlewares/authMiddleware"); // Middleware d'authentification
 
 // Route pour créer une transaction
-router.post("/", transactionController.createTransaction);
+router.post("/", authMiddleware, transactionController.createTransaction);
 
 // Route pour obtenir toutes les transactions pour un compte
-router.get("/:accountId", transactionController.getTransactionsByAccount);
+router.get(
+  "/:accountId",
+  authMiddleware,
+  transactionController.getTransactionsByAccount
+);
 
 // Route pour supprimer une transaction (optionnel)
-router.delete("/:transactionId", transactionController.deleteTransaction);
+router.delete(
+  "/:transactionId",
+  authMiddleware,
+  transactionController.deleteTransaction
+);
 
 module.exports = router;
